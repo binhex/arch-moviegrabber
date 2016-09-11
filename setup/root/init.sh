bf22feb589c473269ad727b9cc5bae540ea9d3f9
binhex <megalith01@gmail.com>
2016-09-11 22:22:47 +0100

    config supervisor for pid 1

--- setup/root/init.sh
+++ setup/root/init.sh
@@ -44,4 +44,4 @@ chmod -R 775 /opt/moviegrabber /home/nobody
 echo "[info] Starting Supervisor..."
 
 # run supervisor
-"/usr/bin/supervisord" -c "/etc/supervisor.conf" -n
+exec /usr/bin/supervisord -c /etc/supervisor.conf -n
