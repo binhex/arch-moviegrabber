73e216b4a4de200b5ae93d4ee1f3ccfb51347d0d
binhex <megalith01@gmail.com>
2017-10-25 13:16:56 +0100

    switch to script pulldown on demand

--- build/root/custom.sh
+++ build/root/custom.sh
@@ -8,7 +8,7 @@ app_name="moviegrabber"
 install_name="moviegrabber"
 
 # download install zip file
-curly.sh -rc 6 -rw 10 -of "/tmp/${app_name}.zip" -url "https://github.com/${repo_name}/${app_name}/archive/development.zip"
+/root/curly.sh -rc 6 -rw 10 -of "/tmp/${app_name}.zip" -url "https://github.com/${repo_name}/${app_name}/archive/development.zip"
 
 # unzip to /tmp
 unzip "/tmp/${app_name}.zip" -d /tmp
