b96cb388d8b2404e9e38d1405df7908649735c77
binhex <megalith01@gmail.com>
2017-12-06 22:16:54 +0000

    switch to github script

--- build/root/install.sh
+++ build/root/install.sh
@@ -44,8 +44,11 @@ aur_packages=""
 # call aur install script (arch user repo)
 source /root/aur.sh
 
-# call custom install script
-source /root/custom.sh
+# github releases
+####
+
+# download sickrage
+/root/github.sh -df "github-download.zip" -dp "/tmp" -ep "/tmp/extracted" -ip "/opt/moviegrabber" -go "binhex" -gr "moviegrabber" -rt "source"
 
 # container perms
 ####
