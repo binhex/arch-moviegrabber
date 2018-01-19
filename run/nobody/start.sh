59190edced907fac6ebe5a27d7e004af96c61edc
binhex <megalith01@gmail.com>
2018-01-19 13:05:46 +0000

    remove pythonpath hack for now

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -5,7 +5,7 @@ install_path="/opt/moviegrabber"
 
 # pex currently has a bug which is preventing local modules from loading, this
 # is the workaround for now (20180117)
-export PYTHONPATH="${install_path}"
+# export PYTHONPATH="${install_path}"
 
 # run moviegrabber using pex package
 "${install_path}/lib/pex/MovieGrabber-linux64.pex" "${install_path}/MovieGrabber.py" --config /config/config --certs /config/certs --db /config/db --logs /config/logs
